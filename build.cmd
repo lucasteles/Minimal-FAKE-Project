2e2061961e702982dd988ba661144be41d0a7a62
Lucas Teles <eu@lucasteles.net>
2018-08-21 20:13:03 -0300

    paket working

---
+++ build.cmd
@@ -0,0 +1,8 @@
+@echo off
+SET TOOL_PATH=.fake
+
+IF NOT EXIST "%TOOL_PATH%\fake.exe" (
+  dotnet tool install fake-cli --tool-path ./%TOOL_PATH%
+)
+
+"%TOOL_PATH%/fake.exe" run build.fsx --target %* 
---
+++ src/Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fake_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
